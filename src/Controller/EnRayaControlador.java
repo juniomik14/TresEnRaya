73c0320e370b3d3917fabc88d8eb01aff54213be
Adriaaltemir <Adriaaltemir18@gmail.com>
2017-12-07 15:31:15 +0100

    creacion atributos controller

--- src/Controller/EnRayaControlador.java
+++ src/Controller/EnRayaControlador.java
@@ -5,11 +5,28 @@
  */
 package Controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import Model.EnRayaModelo;
+import View.EnRayaView;
+
 /**
  *
  * @author Administrador
  */
 public class EnRayaControlador {
+     private EnRayaModelo model;
+    private EnRayaView view;
+
+    public EnRayaControlador(EnRayaModelo model, EnRayaView view) {
+        this.model = model;
+        this.view = view;
+        //Add listeners to the view
+        for (JButton button : view.buttons){
+            button.addActionListener((ActionListener) this);
+        }
+    }
 
 }
 
