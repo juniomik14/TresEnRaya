5d9f767a403c86a9131a17676552ded56776beca
Adriaaltemir <Adriaaltemir18@gmail.com>
2017-12-07 16:16:53 +0100

    Creacion if mensages

--- src/Controller/EnRayaControlador.java
+++ src/Controller/EnRayaControlador.java
@@ -33,10 +33,15 @@ public class EnRayaControlador {
         for (JButton button : view.buttons){
             if (button == ae.getSource()){
                   String s = model.setMovimiento(Integer.parseInt(button.getName()));
-                  //update view
+                  //actualizar view
                   button.setText(s);
-                  //check winner
+                  //mirar ganador
                   int seleccion = -1;
+                  
+                  if( model.getGanador()== 1) {seleccion = view.showMessage(" 'X' ");}
+                  else if( model.getGanador()== 2) {seleccion = view.showMessage(" 'O' ");}
+                  else if(model.getGanador()== 3) {seleccion = view.showMessage(" 'Es un empate' ");}
+            
             }
             
         }
