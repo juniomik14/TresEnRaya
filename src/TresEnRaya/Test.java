6424475efc99d802089a6feaf6e61d092b701a52
juniomik14 <juniomik14@gmail.com>
2017-12-07 14:23:11 +0100

    Main Configurado

--- src/TresEnRaya/Test.java
+++ src/TresEnRaya/Test.java
@@ -1,5 +1,7 @@
 package TresEnRaya;
 
+import View.EnRayaView;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,7 +18,21 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Parte Mike
+        EnRayaView view = new EnRayaView();
+
+
+        
+        //Parte Joel
+        
+        
+        
+        
+        //Parte Adria
+
+
+
+
     }
     
 }
